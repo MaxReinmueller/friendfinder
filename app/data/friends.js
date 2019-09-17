3df9bd244011f1e12c8eed1d30203538973a031c
Max Reinmueller <maxreinmueller@gmail.com>
2019-09-17 14:27:15 -0700

    added in profiles data, made rout to servey page and put in hello world test

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+// Store data in an object
+var profiles = [
+{
+    "name":"Ahmed",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/9/99/Ahmed_Ahmed_2010_Tribeca_FF_Shankbone.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"ryan",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
+    "scores":[
+        5,
+        4,
+        1,
+        3,
+        2,
+        5,
+        3,
+        3,
+        3,
+        1
+      ]
+  },
+  {
+    "name":"eva",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/9/93/EvaMendescropped.jpg",
+    "scores":[
+        5,
+        2,
+        5,
+        2,
+        3,
+        1,
+        2,
+        1,
+        3,
+        5
+      ]
+  }
+]
+// export the object so the rest of the application can use it
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,6 +1,13 @@
 // require in dependencies
 var express = require('express');
+// native node package
 var path = require('path');
 
 // calls the express constructor
 var app = express();
+
+// express get request; app.get(path, callback[callback..])
+exports = app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey"));
+});
+
